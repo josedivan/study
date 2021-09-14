aa1f1e41056f8169dd8f2af02d4a88b233450ba5
josedivan <edivanlima23@gmail.com>
2021-09-14 18:14:32 -0300

    po-select operação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'study';
+
+export class  AppComponent implements OnInit{ 
+
+  resultSom1: number;
+  resultSub1: number;
+  resultMult1: number;
+  resultDiv1: number;
+
+  som1: number;
+  som2: number;
+  sub1: number;
+  sub2: number;
+  mult1: number;
+  mult2: number;
+  div1: number;
+  div2: number;
+
+  a  = 'abelha';
+
+  constructor() {}
+  ngOnInit(): void {
+    console.log( this.a)
+  }
+  sum(som1: number, som2: number) {
+    this.resultSom1 = Number(som1) + Number(som2);
+    console.log(this.resultSom1);
+  }
+
+  sub(sub1: number, sub2: number) {
+    this.resultSub1 = Number(sub1) - Number(sub2);
+    console.log(this.resultSub1);
+  }
+  mult(mult1: number, mult2: number) {
+    this.resultMult1 = Number(mult1) * Number(mult2);
+    console.log(this.resultMult1);
+  }
+
+  div(div1: number, div2: number) {
+    this.resultDiv1 = Number(div1) / Number(div2);
+    console.log(this.resultDiv1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+import { PoButtonModule } from '@po-ui/ng-components';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { PoFieldModule } from '@po-ui/ng-components';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PoModule,
+    PoTemplatesModule,
+    PoButtonModule,
+    PoFieldModule,
+    FormsModule,
+    PoFieldModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
